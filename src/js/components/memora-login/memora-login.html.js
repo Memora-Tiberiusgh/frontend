ec58f16f347faef61c5212fa900aa6471e0f197d
Tiberius Gherac <tg222hh@student.lnu.se>
2025-04-17 20:31:05 +0200

    Initial commit

---
+++ src/js/components/memora-login/memora-login.html.js
@@ -0,0 +1,8 @@
+/**
+ * @file The HTML template for the memora-login web component.
+ * @module memora-login.html
+ */
+// Define the HTML template.
+export const htmlTemplate = document.createElement("template")
+htmlTemplate.innerHTML = `
+`
---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+/**
+ * The main script file of the application that handles component imports and service worker registration.
+ *
+ * This script is responsible for:
+ * - Importing required web components
+ * - Setting up the service worker for offline functionality
+ * - Handling service worker registration events
+ *
+ * @file index.js
+ * @author Tiberius Gherac <tg222hh@student.lnu.se>
+ * @version 1.0.0
+ */
