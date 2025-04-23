73ccc585346a4f73d41070981390ea7f7e720a9d
Tiberius Gherac <tg222hh@student.lnu.se>
2025-04-23 13:40:35 +0200

    Fix communication with back-end and render correctly

--- src/js/components/memora-review/memora-review.html.js
+++ src/js/components/memora-review/memora-review.html.js
@@ -10,20 +10,27 @@ htmlTemplate.innerHTML = `
             <h1 class="memora-collection-name"></h1>
             <div class="memora-collection-meta"></div>
         </div>
-
+        
         <div class="memora-flashcard-container">
             <div class="memora-flashcard">
-                <div class="memora-flashcard-header">
-                    Flashcard
-                </div>
                 <div class="memora-flashcard-body">
-                    <div class="memora-flashcard-question"></div>
-                    <hr class="memora-divider">
-                    <div class="memora-flashcard-answer"></div>
+                    <div class="memora-flashcard-question-container">
+                        <div class="memora-flashcard-question"></div>
+                    </div>
+                    <div class="memora-flashcard-answer-container">
+                        <div class="memora-flashcard-answer"></div>
+                    </div>
                 </div>
                 <div class="memora-flashcard-footer">
-                    <button class="memora-navigation-button prev">Previous Card</button>
-                    <button class="memora-navigation-button next">Next Card</button>
+                    <div class="memora-controls-left">
+                        <button class="memora-navigation-button prev">Previous</button>
+                    </div>
+                    <div class="memora-controls-center">
+                        <button class="memora-reveal-button">Reveal Answer</button>
+                    </div>
+                    <div class="memora-controls-right">
+                        <button class="memora-navigation-button next">Next</button>
+                    </div>
                 </div>
             </div>
         </div>
