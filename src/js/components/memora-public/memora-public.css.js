3077d21e333a574b7819f7722c856dd243791ffa
Tiberius Gherac <tg222hh@student.lnu.se>
2025-05-03 17:29:02 +0200

    Start implementing interface for browsing public collections

---
+++ src/js/components/memora-public/memora-public.css.js
@@ -0,0 +1,10 @@
+/**
+ * @file A CSS template string for the memora-public web component.
+ * @module memora-public.css
+ */
+// Define the CSS template.
+export const cssTemplate = document.createElement("template")
+cssTemplate.innerHTML = `
+<style>
+</style>
+`
